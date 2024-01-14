403e76596ac8a240e071aa58ac375579df188a73
kadenjames <kaden-james@hotmail.com>
2024-01-14 21:06:31 +0000

    Create replace.py
    Learning string structures in python and practised using the replace and upper functions to change the formatting of a string.

---
+++ replace.py
@@ -0,0 +1,14 @@
+# String variable
+string="The!quick!brown!fox!jumps!over!the!lazy!dog."
+
+# Replace the "!" with spaces
+string2=string.replace("!"," ")
+
+# Print the new string
+print(string2)
+
+# Print the new string in upper case
+print(string2.upper())
+
+# Print the string back to front
+print(string2[::-1])
